0b83aa162b973941fab4bbe33be2f98a6006cc7e
An1rey <andrey.ignatuk567@gmail.com>
2023-10-07 10:01:18 +0200

    practice with code

--- app.js
+++ app.js
@@ -26,3 +26,27 @@ function getRandomNumber(min, max) {
 
 const a = getRandomNumber(1, 6);
 slidesPlugin(a);
+
+
+
+
+
+
+// function  slidePlugin(activeSlide = 0){
+//     const slides = document.querySelectorAll('.slide');
+//
+//     slides[activeSlide].classList.add('active')
+//     slides.forEach((slide) => {
+//         slide.addEventListener('click', () => {
+//             clearActiveClasses();
+//             slide.classList.add('active');
+//         });
+//     });
+//
+//     function clearActiveClasses() {
+//         slides.forEach((slide) => {
+//             slide.classList.remove('active');
+//         });
+//     }
+// }
+// slidePlugin(3)
